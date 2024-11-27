eddaf165250f3d50d62800d77486dbaf461f9ce8
Shina Ng <ShinaLNYK@gmail.com>
2024-11-27 10:55:14 -0500

    Add colours to variables.scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import UserHomePage from "./pages/UserHomePage/UserHomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<UserHomePage />} />
                 <Route path="/*" element={<NotFoundPage />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
     return (
-        <header>
-            <h1>My Pet Diary</h1>
-            <button>Log Out</button>
+        <header className="header">
+            <Link className="header__title-link">
+                <h1 className="header__title">My Pet Diary</h1>
+            </Link>
+            <button className="header__log-out-btn">Log Out</button>
         </header>
     );
 }
