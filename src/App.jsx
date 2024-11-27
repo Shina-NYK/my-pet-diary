dbbfda7e0fa52a580237c83775b3d5eb107f1d91
Shina Ng <ShinaLNYK@gmail.com>
2024-11-27 17:38:14 -0500

    Move data fetching to App.jsx to minimize repeating API calls. EntryMenu working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,70 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.scss";
 import UserHomePage from "./pages/UserHomePage/UserHomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import Header from "./components/Header/Header";
 
 function App() {
+    const baseApiUrl = import.meta.env.VITE_API_URL;
+    const [years, setYears] = useState([]);
+    const [yearSelected, setYearSelected] = useState([]);
+
+    // Sets the year selected to the year clicked
+    const handleYearClick = (year) => {
+        // Check if the year selected is included in the expanded list
+        setYearSelected((prevState) => {
+            // keeps the previously expanded years state as is and removes the year selected if already resent in the list
+            if (prevState.includes(year)) {
+                return prevState.filter(
+                    (yearsExpanded) => yearsExpanded !== year
+                );
+
+                // adds the year selected to the list of years to expand
+            } else {
+                return [...prevState, year];
+            }
+        });
+    };
+
+    // Get year data for entries
+    const getAllYears = async () => {
+        try {
+            const { data } = await axios.get(`${baseApiUrl}/entries`);
+            setYears(data);
+        } catch (error) {}
+    };
+
+    useEffect(() => {
+        getAllYears();
+    }, []);
+
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/" element={<UserHomePage />} />
-                <Route path="/user" element={<UserHomePage />} />
+                {/* Placeholder element until registration page is implemented */}
+                <Route
+                    path="/"
+                    element={
+                        <UserHomePage
+                            years={years}
+                            handleYearClick={handleYearClick}
+                            yearSelected={yearSelected}
+                        />
+                    }
+                />
+                <Route
+                    path="/user"
+                    element={
+                        <UserHomePage
+                            years={years}
+                            handleYearClick={handleYearClick}
+                            yearSelected={yearSelected}
+                        />
+                    }
+                />
                 <Route path="/*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
--- src/pages/UserHomePage/UserHomePage.jsx
+++ src/pages/UserHomePage/UserHomePage.jsx
@@ -1,42 +1,10 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import EntryMenu from "../../components/EntryMenu/EntryMenu";
 import "./UserHomePage.scss";
 
-function UserHomePage() {
-    const baseApiUrl = import.meta.env.VITE_API_URL;
-    const [years, setYears] = useState([]);
-    const [yearSelected, setYearSelected] = useState([]);
-
-    // Sets the year selected to the year clicked
-    const handleYearClick = (year) => {
-        // Check if the year selected is included in the expanded list
-        setYearSelected((prevState) => {
-            // keeps the previously expanded years state as is and removes the year selected if already resent in the list
-            if (prevState.includes(year)) {
-                return prevState.filter(
-                    (yearsExpanded) => yearsExpanded !== year
-                );
-
-                // adds the year selected to the list of years to expand
-            } else {
-                return [...prevState, year];
-            }
-        });
-    };
-
-    // Get year data for entries
-    const getAllYears = async () => {
-        try {
-            const { data } = await axios.get(`${baseApiUrl}/entries`);
-            setYears(data);
-        } catch (error) {}
-    };
-
-    useEffect(() => {
-        getAllYears();
-    }, []);
-
+function UserHomePage({ years, handleYearClick, yearSelected }) {
     return (
         <main className="userHp__main">
             <EntryMenu
