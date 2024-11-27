e5ac496cb651ebee2e97a148b380f4ce5796b6b6
Shina Ng <ShinaLNYK@gmail.com>
2024-11-27 15:33:02 -0500

    Set up list of years from backend. Add icons folder in assets

--- src/components/EntryMenu/EntryMenu.jsx
+++ src/components/EntryMenu/EntryMenu.jsx
@@ -1,19 +1,20 @@
 import "./EntryMenu.scss";
+import arrowUp from "../../assets/Icons/arrow_drop_up.svg";
+import arrowDown from "../../assets/Icons/arrow_drop_down.svg";
 
-function EntryMenu() {
+function EntryMenu({ years }) {
     return (
         <section className="menu">
             <h2 className="menu__header">Your Moments</h2>
             {/* List of entries by year and month */}
             <ul className="menu__year-list">
-                <li className="menu__year-item">
-                    <h3 className="dropdown__text">2023</h3>
-                    <span className="dropdown__indicator">▼</span>
-                </li>
-                <li className="menu__year-item">
-                    <h3 className="dropdown__text">2024</h3>
-                    <span className="dropdown__indicator">▼</span>
-                </li>
+                {years.map((year) => (
+                    <li className="menu__year-item" key={year.year}>
+                        <h3 className="dropdown__text">{year.year}</h3>
+                        <img className="dropdown__indicator" src={arrowUp} />
+                        <img className="dropdown__indicator" src={arrowDown} />
+                    </li>
+                ))}
             </ul>
         </section>
     );
--- src/pages/UserHomePage/UserHomePage.jsx
+++ src/pages/UserHomePage/UserHomePage.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import EntryMenu from "../../components/EntryMenu/EntryMenu";
 import "./UserHomePage.scss";
 
 function UserHomePage() {
     const baseApiUrl = import.meta.env.VITE_API_URL;
+    const [years, setYears] = useState([]);
     const [yearExpanded, setYearExpanded] = useState(null);
 
     const yearSelected = (year) => {
@@ -12,9 +14,22 @@ function UserHomePage() {
 
     const handleYearClick = () => {};
 
+    // Get year data for entries
+    const getAllYears = async () => {
+        try {
+            const { data } = await axios.get(`${baseApiUrl}/entries`);
+            console.log(data);
+            setYears(data);
+        } catch (error) {}
+    };
+
+    useEffect(() => {
+        getAllYears();
+    }, []);
+
     return (
         <main className="userHp__main">
-            <EntryMenu />
+            <EntryMenu years={years} />
         </main>
     );
 }
